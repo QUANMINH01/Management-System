60bfe4e20dd153f7f120d12fdfdbb5a7b7a05d9a
minhquan <minhquan123dn02@gmail.com>
2025-08-15 19:32:40 +0700

    Fix bcrypt issue: replace bcrypt with bcryptjs and remove node_modules

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,7 +3,7 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 import { connectToDatabase } from "../lib/db.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const router = express.Router();
